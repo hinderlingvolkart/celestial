dbd2a3257bb170e67ebd1f8f87841b72155c4b82
Philip Ullrich <pul@hinderlingvolkart.com>
2024-04-23 14:13:08 +0200

    feat: allow custom header/footer for stories

--- src/lib/generators/generateDocsPage.ts
+++ src/lib/generators/generateDocsPage.ts
@@ -1,17 +1,27 @@
 import path from "node:path";
 import type {CelestialDoc} from "../../types.ts";
-import {STYLEGUIDE_LAYOUT_DIR} from "../../helpers/constants.js";
+import {STYLEGUIDE_LAYOUT_DIR, WORKSPACE_ROOT} from "../../helpers/constants.js";
 import {getParamsForSubdoc} from "../../helpers/string.js";
+import {existsSync} from "node:fs";
 
 export function generateDocsPage(targetPath: string, doc: CelestialDoc): string {
-    const layoutPath = path.join(STYLEGUIDE_LAYOUT_DIR, `LayoutInner.astro`)
+    const layoutPath = path.join(STYLEGUIDE_LAYOUT_DIR, `LayoutInner.astro`);
+    const customHeadPath = path.join(WORKSPACE_ROOT, `celestial.doc-head.astro`);
+    const customFooterPath = path.join(WORKSPACE_ROOT, `celestial.doc-footer.astro`);
     const relativeLayoutPath = path.relative(targetPath, layoutPath);
     const relativeStoryPath = path.relative(targetPath, doc.docPath);
+    const relativeCustomHeadPath = path.relative(targetPath, customHeadPath);
+    const relativeCustomFooterPath = path.relative(targetPath, customFooterPath);
+
+    const hasCustomHead = existsSync(customHeadPath);
+    const hasCustomFooter = existsSync(customFooterPath);
 
     return `---
 import type { GetStaticPaths } from "astro";
 import Layout from "${relativeLayoutPath}";
 import Page from "${relativeStoryPath}";
+${ hasCustomHead ? 'import CustomHead from "' + relativeCustomHeadPath + '";' : 'const CustomHead = null;'};
+${ hasCustomFooter ? 'import CustomFooter from "' + relativeCustomFooterPath + '";' : 'const CustomFooter = null;'};
 
 export const getStaticPaths = (() => {
   return [
@@ -20,5 +30,13 @@ export const getStaticPaths = (() => {
 }) satisfies GetStaticPaths;
 ---
 <Layout title="${doc.name}">
- <Page />
+  <Page />
+
+  { CustomHead && <Fragment slot="head">
+    <CustomHead />
+  </Fragment> }
+
+  { CustomFooter && <Fragment slot="footer">
+    <CustomFooter />
+  </Fragment> }
 </Layout>`;}
